573395ad3bd89f75e14af080944e6bf199e701ec
Artem Onyshchenko <artemon888.com@gmail.com>
2025-11-12 11:12:31 +0200

    Updated site

--- script.js
+++ script.js
@@ -16,3 +16,4 @@ const onPressedAboutme = () => window.open('https://t.me/Artemon0000', '_blank')
 const onPressedProjects = () => window.open('https://github.com/Artemon0?tab=repositories', '_blank');
 const onPressedTg = () => window.open('https://t.me/AOGames888', '_blank');
 const onPressedFunctions = () => window.open('https://github.com/Artemon0/UsableFunctions#Functions', '_blank');
+const onPressedSite  = () => window.open('https://artemon0.github.io/UsableFunctions/', '_blank');
